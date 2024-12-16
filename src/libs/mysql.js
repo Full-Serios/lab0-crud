8f5abb352c3ac53014e923a1ea42096cc450d64b
dieramirezma <dieramirezma@unal.edu.co>
2024-12-16 18:54:12 -0500

    feat: add mysql2 dependency and update database connection configuration

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -1,11 +1,12 @@
 import mysql from 'serverless-mysql'
 
 export const dbConnection = mysql({
-    config: {
-        host: process.env.MYSQL_ADDON_HOST,
-        user: process.env.MYSQL_ADDON_USER,
-        password: process.env.MYSQL_ADDON_PASSWORD,
-        port: process.env.MYSQL_ADDON_PORT,
-        database: process.env.MYSQL_ADDON_DB
-    }
+  library: require('mysql2'),
+  config: {
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    port: process.env.MYSQL_ADDON_PORT,
+    database: process.env.MYSQL_ADDON_DB
+  }
 })
